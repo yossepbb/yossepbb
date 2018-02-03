a4d61a8774bbaa384531eff933267aee74e42ae4
Joseph Binyoum Bayemi <b.bj@hotmail.fr>
2018-02-03 15:50:51 +0100

    bookcommunity

---
+++ Desktop/batch-127/intro.rb
@@ -0,0 +1,43 @@
+# ceci est un commentaire
+=begin
+
+learn enough command lines to be dangerous
+
+la --- montre les fichiers cachés
+
+git init ---- creer une photo dans git
+
+git status ----- montre ts mes fichiers
+
+git add --- pr ajouter un fichier
+
+git add . --- ajoute ts les fichiers du repertoire
+
+git commit --message"TRES IMPORTANT MESSAGE COHERENT"-- enregister les modifications
+
+ ou
+
+git commit -m" toto"
+
+git diff ---- pr voir fichier modifier
+
+git log --- pr voir historique
+
+git commit --amend --- permet de modifie e dernier commit
+
+TRAVAILLER EN LIGNE
+
+FORK AND CLONE
+
+git clone -----  telechargement en local  du projet
+
+fork ---copy projet d'un compte git vers un autre compte github
+
+
+git push remote branch
+
+remote est le serveur exple github
+
+branch est le master
+
+end
